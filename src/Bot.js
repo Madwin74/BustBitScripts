4ee84d658872804d90046ee3e0f75373b3ea7b70
merowinger92 <makro9267@gmail.com>
2016-01-22 18:55:09 +0100

    Main Bot File

---
+++ src/Bot.js
@@ -0,0 +1,57 @@
+/* MODULES
+-----------------*/
+
+
+/* TEMP. VARS
+-----------------*/
+
+
+/* EVENTS
+-----------------*/
+
+
+/* BOT
+-----------------*/
+function MeroBot(){
+    var self = this
+    self.Config = require('./Config');
+    //console.log(JSON.stringify(self.Config)); // uncomment to see your configs
+    var GameClient = require('./GameClient'),
+        WebClient = require('./WebClient');
+    
+    // Set bot's session cookie for connections
+    require('socket.io-client-cookie').setCookies('id=' + self.Config.SESSION);
+    
+    // Connect to the game server.
+    self.gameClient = new GameClient(self.Config);
+    
+    // Connect to the web server.
+    self.webClient = new WebClient(self.Config);
+    
+    //register functions on Events game_starting & player_cashed_out
+    
+    
+    });
+    
+}
+module.exports.merobot = new MeroBot();
+
+
+/*EVENTS 
+-------------------*/
+
+//gamestarting logic
+
+
+//player_cashed_out logic
+
+
+
+
+/* UNCAUGHT EXCEPTIONS
+-----------------*/
+process.on('uncaughtException', function(err) {
+    console.error((new Date).toUTCString() + ' uncaughtException:', err.message);
+    console.error(err.stack);
+    process.exit(1);
+});
