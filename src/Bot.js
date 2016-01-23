d6f43fcf7362610275bc5a64a84fe2097e10dbf6
merowinger92 <makro9267@gmail.com>
2016-01-23 16:47:48 +0100

    Update Bot.js

--- src/Bot.js
+++ src/Bot.js
@@ -86,7 +86,7 @@ function MeroBot(){
     //register function on Event game_crashed
     self.gameClient.on('game_crash', function(data){
         console.log(JSON.stringify(data));
-        console.log(game_crash);
+        console.log(data.game_crash);
         if (playedGames > 0 && played)
 		{
 		    played = false;
