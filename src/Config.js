3cd4f4ca19f867a968d267a8a09a8f121d2772ee
merowinger92 <makro9267@gmail.com>
2016-01-24 19:41:55 +0100

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 module.exports = {
     GAMESERVER: "https://gs.bustabit.com",
     WEBSERVER: "https://www.bustabit.com",
-    SESSION: "c96c1efc-0b99-4970-b895-002d44e1bc5d",
+    SESSION: "",
     USER: "merowinger92",
-    PASSWORD: "scheune126"
+    PASSWORD: ""
 };
