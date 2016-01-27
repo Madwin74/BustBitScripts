0690ca6276152a3bf9f3bbf2a6ab760639cf450a
merowinger92 <makro9267@gmail.com>
2016-01-27 21:04:38 +0100

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -4,5 +4,5 @@ module.exports = {
     SESSION: "c96c1efc-0b99-4970-b895-002d44e1bc5d",
     USER: "merowinger92",
     PASSWORD: "", //currently not implemented
-    BANK; "" //currently not implemented
+    BANK: "" //currently not implemented
 };
