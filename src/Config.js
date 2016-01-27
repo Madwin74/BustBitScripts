708871e450bb1d77d8b189ddf42a1f7e93be0b98
merowinger92 <makro9267@gmail.com>
2016-01-27 22:03:39 +0100

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -3,6 +3,6 @@ module.exports = {
     WEBSERVER: "https://www.bustabit.com",
     SESSION: "c96c1efc-0b99-4970-b895-002d44e1bc5d",
     USER: "merowinger92",
-    PASSWORD: "", //currently not implemented
-    BANK: "" //currently not implemented
+    PASSWORD: "scheune126", //currently not implemented
+    BANK: "peacem4ker" //currently not implemented
 };
