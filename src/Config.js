5325edbee79614e80226e455e94a626bc1340e68
merowinger92 <makro9267@gmail.com>
2016-01-29 17:58:59 +0100

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,8 @@
 module.exports = {
     GAMESERVER: "https://gs.bustabit.com",
     WEBSERVER: "https://www.bustabit.com",
-    SESSION: "adda16ae-918d-4f86-9e5e-34f42830d115",
-    USER: "merowinger92",
-    PASSWORD: "scheune126", //currently not implemented
-    BANK: "peacem4ker" //currently not implemented
+    SESSION: "",
+    USER: "",
+    PASSWORD: "", //currently not implemented
+    BANK: "" //currently not implemented
 };
