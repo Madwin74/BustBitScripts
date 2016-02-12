ba08e21fda5d9bb39c29976a67bf953c91b5ce7d
merowinger92 <makro9267@gmail.com>
2016-02-12 23:31:21 +0100

    Update GameConfig.js

--- src/GameConfig.js
+++ src/GameConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TARGET: 1.13, //Target Multiplier when to cash out
+    TARGET: 1.25, //Target Multiplier when to cash out
     BET: 1, //first BET
     MAX: 10000000000000, //maximum bet that the bot will bet
     RESET: false, //true: if max bet is reached bet will be reset to initial bet, false: continue with maxBet 
@@ -7,9 +7,9 @@ module.exports = {
     LOW: false, //true: stop if balance is not enough for currentBet, false: continue with intial bet if possible
     PROFIT: 1000, //Target Profit in bits; don't be too greedy
     LOSSSTREAKPROTECTION: true, // enable LossStreak Protection
-    LOSSSTREAK: 3, //if Loss Streak Protection is enabled; maximum lose streak before pausing the bot for x games
+    LOSSSTREAK: 2, //if Loss Streak Protection is enabled; maximum lose streak before pausing the bot for x games
     MULTIPLY: false, // if set to true each winning stake is multiplied by betMultiplier
     MULITIPLIER: 2, //only used when multilyBet is true
-    ENABLEBANK: true, //enable for save x amount of profit to another account
+    ENABLEBANK: false, //enable for save x amount of profit to another account
     BANK: 100 //amount when to save profit
 };
